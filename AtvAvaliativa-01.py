42e1c202f390f2e37008cb94640e9a7ce3f3dcf1
Padreeca <g.padrecanicoletti@gmail.com>
2025-04-13 21:00:01 -0300

    Faltante: limitar input, bug 4 numeros iguais

--- AtvAvaliativa-01.py
+++ AtvAvaliativa-01.py
@@ -29,50 +29,75 @@ while jogando != 0:
 
         if count == 5:
             print("\n dica")
-            while True:        #tirar while true
+            dica = 0
+            while dica == 0:        #tirar while true
                 n_dica = random.randint(1,4)
-
+                par_impar_maior_menor = random.randint(1,2)
                 if n_dica==1 and not pc1:     #se random ==1
-                    if pc1%2==0:
-                        pc1="par"
+                    if par_impar_maior_menor == 1:
+                        if p1%2==0:
+                            print("Primeira posição é par")
+                            dica = 1
+                        else:
+                            print("Primeira posição é ímpar")
+                            dica = 1
                     else:
-                        pc1="ímpar"
-                    if pc1<5:
-                        pc1="<5"
-                    else:
-                        pc1=">4"
+                        if pc1<5:
+                            print("Primeira posição menor que 5")
+                            dica = 1
+                        else:
+                            print("Primeira posição maior que 4")
+                            dica = 1
 
                 if n_dica==2 and not pc2:     #se random ==2
-                    if pc2%2==0:
-                        pc2="par"
+                    if par_impar_maior_menor == 1:
+                        if pc2%2==0:
+                            print("Segunda posição é par")
+                            dica = 1
+                        else:
+                            print("Segunda posição é ímpar")
+                            dica = 1
                     else:
-                        pc2="ímpar"
-                    if pc2<5:
-                        pc2="<5"
-                    else:
-                        pc2=">4"
+                        if pc2<5:
+                            print("Segunda posição menor que 5")
+                            dica = 1
+                        else:
+                            print("Segunda posição maior que 4")
+                            dica = 1
 
                 if n_dica==3 and not pc3:     #se random ==3
-                    if pc3%2==0:
-                        pc3="par"
+                    if par_impar_maior_menor == 1:
+                        if pc3%2==0:
+                            print("Terceira posição é par")
+                            dica = 1
+                        else:
+                            print("Teceira posição é ímpar")
+                            dica = 1
                     else:
-                        pc3="ímpar"
-                    if pc3<5:
-                        pc3="<5"
-                    else:
-                        pc3=">4"
+                        if pc3<5:
+                            print("Terceira posição menor que 5")
+                            dica = 1
+                        else:
+                            print("Terceira posição maior que 4")
+                            dica = 1
 
-                if n_dica==3 and not pc4:     #se random ==4
-                    if pc4%2==0:
-                        pc4="par"
+                if n_dica==4 and not pc4:     #se random ==4
+                    if par_impar_maior_menor == 1:
+                        if pc4%2==0:
+                            print("Quarta posição é par")
+                            dica = 1
+                        else:
+                            print("Quarta posição é ímpar")
+                            dica = 1
                     else:
-                        pc4="ímpar"
-                    if pc4<5:
-                        pc4="<5"
-                    else:
-                        pc4=">4"        
+                        if pc4<5:
+                            print("Quarta posição menor que 5")
+                            dica = 1
+                        else:
+                            print("Quarta posição maior que 4")
+                            dica = 1       
 
-        pc=0
+        pe=0
         pc1=False
         pc2=False
         pc3=False
@@ -84,38 +109,37 @@ while jogando != 0:
         p1 = palpite/1000
         p1 = int(p1)
 
-        if p1 == n1 or p1==n2 or p1==n3 or p1==n4:
-            pc+=1
-            if p1 == n1:
-                pc1=True 
+        if p1 == n1:
+            pc1 = True    #posição correta do 1
+        elif p1 == n2 or p1 == n3 or p1 == n4:
+            pe += 1
 
         p2 = (palpite%1000)/100
         p2 = int(p2)
-        if p2 == n1 or p2==n2 or p2==n3 or p2==n4:
-            pc+=1
-            if p2 == n2:
-                pc2=True
+        if p2 == n2:
+            pc2 = True
+        elif p2 == n1 or p2 == n3 or p2 == n4:
+            pe += 1
 
         p3 = (palpite%100)/10
         p3 = int(p3)
-        if p3 == n1 or p3==n2 or p3==n3 or p3==n4:
-            pc+=1
-            if p3 == n3:
-                pc3=True
+        if p3 == n3:
+            pc3 = True
+        elif p3 == n1 or p3 == n2 or p3 == n4:
+            pe += 1
 
         p4 = palpite%10
-        if p4 == n1 or p4==n2 or p4==n3 or p4==n4:
-            pc+=1
-            if p4 == n4:
-                pc4=True
-            
+        if p4 == n4:
+            pc4 = True
+        elif p4 == n1 or p4 == n2 or p4 == n3:
+            pe += 1
             
 
         count+=1
         print(count,"° tentativa: \n")
 
-        print(pc," números CORRETOS")
-        # pe = 4-pc
+        print(pe," números CORRETOS na POSIÇÃO INCORRETA")
+        # pe = 4-pe
         # print(pe," números na posição ERRADA\n")
 
         #printa quais números corretos
